978e865990d381ada6e95305f9a10ed068aafdd6
SathvikSesha <lakshmisarmatk@gmail.com>
2026-07-03 20:43:29 +0530

    controller: Implement getDeals and getDealById controllers

--- dealflow-backend/controllers/dealController.js
+++ dealflow-backend/controllers/dealController.js
@@ -32,3 +32,61 @@ export const createDeal = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getDeals = async (req, res) => {
+  try {
+    let deals = [];
+    if (req.user.role === "ADMIN" || req.user.role === "EXECUTIVE") {
+      deals = await Deal.find({ companyId: req.user.companyId }).sort({
+        createdAt: -1,
+      });
+    } else {
+      const memberships = await DealMember.find({ userId: req.user._id });
+      const assignedDealIds = memberships.map((member) => member.dealId);
+      deals = await Deal.find({
+        _id: { $in: assignedDealIds },
+        companyId: req.user.companyId,
+      }).sort({ createdAt: -1 });
+    }
+    return res.status(200).json(deals);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getDealById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deal = await Deal.findOne({ _id: id, companyId: req.user.companyId });
+
+    if (!deal) {
+      return res
+        .status(404)
+        .json({ message: "Deal not found in your workspace." });
+    }
+
+    if (req.user.role !== "ADMIN" && req.user.role !== "EXECUTIVE") {
+      const isAssigned = await DealMember.findOne({
+        dealId: id,
+        userId: req.user._id,
+      });
+
+      if (!isAssigned) {
+        return res.status(403).json({
+          message:
+            "Access denied. You are not assigned to this deal workspace.",
+        });
+      }
+    }
+
+    const members = await DealMember.find({ dealId: id })
+      .populate("userId", "name email role inviteStatus isActive")
+      .sort({ assignedAt: -1 });
+    res.status(200).json({
+      deal,
+      members,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
